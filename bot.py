fa279fa46afc441fcacdaf6538af87b6dd4e4128
fishguy6564 <fishguy6564@gmail.com>
2020-02-08 08:27:55 +0000

    Replace bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
 import asyncio
+import sys
 
-TOKEN = 'YOUR DISCORD BOT TOKEN HERE'
+TOKEN = 'INPUT YOUR BOT TOKKEN HERE'
 
 client = commands.Bot(command_prefix = '$')
 client.remove_command('help')
@@ -11,44 +12,57 @@ extensions = ['RaidCommands']
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
 
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	channel = message.channel
+	if message.guild is None:
+		await message.channel.send("Hey there!")
+	await client.process_commands(message)
+
+#Command used for bot admin to turn their bot off
+#Please put the admin's discord ID where indicated
 @client.command()
 async def logout(ctx):
-    if ctx.message.author.id in [YOUR DISCORD ID HERE]:
-        await ctx.send('```Shutting down...```')
-        await client.logout()
-    else:
-        await ctx.send('Nice try jackass!')
+	if ctx.message.author.id in [ADMINS DISCORD ID AS INT]:
+		await ctx.send('```Shutting down...```')
+		await client.logout()
+	else:
+		await ctx.send('Nice try jackass!')
 
+#Sends greet command
 @client.command()
 async def greet(ctx):
-	await ctx.send("Hello everyone! I am Lanturn and I'm here to assist you :)")
+	await ctx.send("Hello everyone! I am <placeholder> and I'm here to assist you :)")
 
 @client.command()
 async def load(extension):
-    try:
-        client.load_extension(extension)
-        print('Loaded {}'.format(extension))
-    except Exception as error:
-        print('{} cannot be loaded. [{}]'.format(extension, error))
+	try:
+		client.load_extension(extension)
+		print('Loaded {}'.format(extension))
+	except Exception as error:
+		print('{} cannot be loaded. [{}]'.format(extension, error))
 
 @client.command()
 async def unload(extension):
-    try:
-        client.unload_extension(extension)
-        print('Unloaded {}'.format(extension))
-    except Exception as error:
-        print('{} cannot be unloaded. [{}]'.format(extension, error))
+	try:
+		client.unload_extension(extension)
+		print('Unloaded {}'.format(extension))
+	except Exception as error:
+		print('{} cannot be unloaded. [{}]'.format(extension, error))
 
 if __name__ == '__main__':
-    for extension in extensions:
-        try:
-            client.load_extension(extension)
-        except Exception as error:
-            print('{} cannot be loaded. [{}]'.format(extension, error))
+	for extension in extensions:
+		try:
+			client.load_extension(extension)
+		except Exception as error:
+			print('{} cannot be loaded. [{}]'.format(extension, error))
 
-    client.run(TOKEN)
+	#client.loop.create_task(test())
+	client.run(TOKEN)
